e47f3300010f4f1f2ed351e6262b23bc7b58a7d4
sulaiman1808 <55393597+sulaiman1808@users.noreply.github.com>
2019-10-01 23:06:58 +0500

    Add files via upload
    Counts the number of alphabets and nonalphabets in a string entered by the user

---
+++ Alphabet and Non Alphabet Counter.vb
@@ -0,0 +1,44 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim String1, NextChar As String
+        Dim AlphabetCount, NonAlphabetCount As Integer
+
+
+        String1 = ""
+        NextChar = ""
+        AlphabetCount = 0
+        NonAlphabetCount = 0
+
+
+        Console.Write("Enter String To Be Processed:")
+        String1 = Console.ReadLine
+
+
+        For Count = 1 To Len(String1)
+            NextChar = Mid(String1, Count, 1)
+
+            If NextChar >= "A" And NextChar <= "z" Then
+                AlphabetCount = AlphabetCount + 1
+
+
+            Else
+                NonAlphabetCount = Len(String1) - AlphabetCount
+
+            End If
+
+
+        Next
+
+        Console.WriteLine("AlphabetCount= " & AlphabetCount)
+        Console.WriteLine("NonAlphabetCount= " & NonAlphabetCount)
+        Console.ReadKey()
+
+
+
+
+
+    End Sub
+
+End Module

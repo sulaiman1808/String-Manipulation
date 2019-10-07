5c75a8e63208265ca73e838a483a8658783c2281
sulaiman1808 <55393597+sulaiman1808@users.noreply.github.com>
2019-10-07 21:27:03 +0500

    Truncates A Specific Character From A String

---
+++ CharacterTruncation.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MainString As String = ""
+        Dim FinalString As String = ""
+        Dim CharacterToBeRemoved As Char = ""
+        Dim NextChar As Char = ""
+        Dim Counter As Integer = 0
+
+        Console.Write("Enter String To Be Processed: ")
+        MainString = Console.ReadLine
+
+        Console.Write("Enter Character To Be Truncated: ")
+        CharacterToBeRemoved = Console.ReadLine
+
+        For Counter = 1 To Len(MainString)
+            NextChar = Mid(MainString, Counter, 1)
+            If NextChar <> CharacterToBeRemoved Then
+                FinalString = FinalString & NextChar
+            End If
+        Next
+
+        Console.WriteLine("After Truncation Of" & " " & CharacterToBeRemoved & " " & "The Final String Is " & FinalString)
+        Console.ReadKey()
+
+    End Sub
+
+End Module

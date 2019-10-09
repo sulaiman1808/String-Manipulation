9c4adbebe20732730c32e48adbf9512bb27500ce
sulaiman1808 <55393597+sulaiman1808@users.noreply.github.com>
2019-10-09 19:09:14 +0500

    Isolate Different Data Types Such As Letters, Numbers & Special Chracters

---
+++ IsolateDataTypes.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MainString As String = ""
+        Dim Counter As Integer = 0
+        Dim NextChar As String = ""
+        Dim WORDS As String = ""
+        Dim NUMBERS As String = 0
+        Dim SPECIALCHARACTERS As String = ""
+
+        Console.Write("Enter String To Be Processed: ")
+        MainString = Console.ReadLine
+
+        For Counter = 1 To Len(MainString)
+            NextChar = Mid(MainString, Counter, 1)
+            If (NextChar >= "A" And NextChar <= "Z") Or (NextChar >= "a" And NextChar <= "z") Then
+                WORDS = WORDS + NextChar
+            ElseIf (NextChar >= "0" And NextChar <= "9") Then
+                NUMBERS = NUMBERS + NextChar
+            Else
+                SPECIALCHARACTERS = SPECIALCHARACTERS + NextChar
+            End If
+        Next
+        Console.WriteLine("WORDS: " & WORDS)
+        Console.WriteLine("NUMBERS: " & NUMBERS)
+        Console.WriteLine("SPECIALCHARACTERS: " & SPECIALCHARACTERS)
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module

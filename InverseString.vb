57a72cf9a85ea5b0d29fe9b88ae7eed5a1bcc06b
sulaiman1808 <55393597+sulaiman1808@users.noreply.github.com>
2019-10-09 21:58:49 +0500

    Inverses A Given String

---
+++ InverseString.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MainString As String = ""
+        Dim Counter As Integer = 0
+        Dim NextChar As Char = ""
+        Dim InversedString As String = ""
+
+        Console.Write("Enter String To Inverse: ")
+        MainString = Console.ReadLine
+
+        For Counter = Len(MainString) To 1 Step -1
+            NextChar = Mid(MainString, Counter, 1)
+            InversedString = InversedString + NextChar
+        Next
+
+        Console.WriteLine("INVERSED STRING IS " & InversedString)
+        Console.ReadKey()
+
+    End Sub
+
+End Module

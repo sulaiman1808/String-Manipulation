c62da730819ff6cb9af5d649c5b857ad0cc19221
sulaiman1808 <55393597+sulaiman1808@users.noreply.github.com>
2019-10-09 23:11:30 +0500

    Isolates Words Of An Entered String

---
+++ IsolateWords.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MainString As String = ""
+        Dim Counter As Integer = 0
+        Dim NextChar As Char = ""
+        Dim FinalString As String = ""
+
+        Console.Write("Enter String To Isolate Words: ")
+        MainString = Console.ReadLine
+
+        For Counter = 1 To Len(MainString)
+            NextChar = Mid(MainString, Counter, 1)
+            If NextChar <> " " Then
+                FinalString = FinalString + NextChar
+            End If
+            If Counter = Len(MainString) Or NextChar = " " Then
+                Console.WriteLine(FinalString)
+                FinalString = ""
+            End If
+        Next Counter
+        Console.ReadKey()
+
+
+
+
+    End Sub
+
+End Module

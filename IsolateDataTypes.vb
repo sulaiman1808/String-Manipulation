346f5cafe45ac0802f4e235c9648f2e5dcc5dd3f
sulaiman1808 <55393597+sulaiman1808@users.noreply.github.com>
2019-10-09 19:22:32 +0500

    Update IsolateDataTypes.vb

--- IsolateDataTypes.vb
+++ IsolateDataTypes.vb
@@ -1,10 +1,11 @@
-﻿Module Module1
+Module Module1
 
     Sub Main()
+
         Dim MainString As String = ""
         Dim Counter As Integer = 0
         Dim NextChar As String = ""
-        Dim WORDS As String = ""
+        Dim LETTERS As String = ""
         Dim NUMBERS As String = 0
         Dim SPECIALCHARACTERS As String = ""
 
@@ -14,19 +15,19 @@
         For Counter = 1 To Len(MainString)
             NextChar = Mid(MainString, Counter, 1)
             If (NextChar >= "A" And NextChar <= "Z") Or (NextChar >= "a" And NextChar <= "z") Then
-                WORDS = WORDS + NextChar
+                LETTERS = LETTERS + NextChar
             ElseIf (NextChar >= "0" And NextChar <= "9") Then
                 NUMBERS = NUMBERS + NextChar
             Else
                 SPECIALCHARACTERS = SPECIALCHARACTERS + NextChar
             End If
         Next
-        Console.WriteLine("WORDS: " & WORDS)
+    Console.WriteLine("LETTERS: " & LETTERS)
         Console.WriteLine("NUMBERS: " & NUMBERS)
         Console.WriteLine("SPECIALCHARACTERS: " & SPECIALCHARACTERS)
         Console.ReadKey()
 
-
     End Sub
 
 End Module
+

bc7742a86824b79a082b4cde5bb051b7133d271d
sulaiman1808 <55393597+sulaiman1808@users.noreply.github.com>
2019-10-13 23:09:53 +0500

    jhhfjhyhjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj

---
+++ Fiestnamelastname.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim FirstString, SecondString, FirstName, LastName As String
+        Dim SpacePosition As Integer
+
+
+        FirstString = ""
+        SecondString = ""
+        SpacePosition = 0
+        FirstName = ""
+        LastName = ""
+
+
+        Console.Write("Enter First String To Be Processed: ")
+        FirstString = Console.ReadLine()
+
+        SpacePosition = InStr(FirstString, " ")
+        FirstName = Mid(FirstString, 1, SpacePosition)
+
+        Console.Write("Enter Second String To Be Processed: ")
+        SecondString = Console.ReadLine
+
+        SpacePosition = InStr(SecondString, " ")
+        LastName = Mid(SecondString, SpacePosition + 1, Len(SecondString))
+
+        Console.WriteLine("Final String: " & FirstName & " " & LastName)
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
